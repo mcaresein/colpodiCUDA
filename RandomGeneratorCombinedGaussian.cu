2529435e23fc7fe45a4ee8a4091ecc92d1bf412d
azzauzuaz <38854449+azzauzuaz@users.noreply.github.com>
2018-09-08 19:31:38 +0200

    Update RandomGeneratorCombinedGaussian.cu

--- RandomGeneratorCombinedGaussian.cu
+++ RandomGeneratorCombinedGaussian.cu
@@ -1,5 +1,5 @@
 #include "RandomGeneratorCombinedGaussian.h"
-#include <iostream>
+
 __host__ __device__ double RandomGeneratorCombinedGaussian::GetRandomNumber(){
   return this->GetGaussianRandomNumber(this);
 };
