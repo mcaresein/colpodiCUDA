6ef38005213286b4409988d4de26aa53a84e4db4
azzauzuaz <38854449+azzauzuaz@users.noreply.github.com>
2018-09-10 11:39:26 +0200

    Add files via upload

--- MonteCarloPricer.cu
+++ MonteCarloPricer.cu
@@ -13,10 +13,13 @@ __device__ __host__ MonteCarloPricer::MonteCarloPricer(Option* Option, MonteCarl
 __device__ __host__ void MonteCarloPricer::ComputePrice(Statistics* PayOffs){
     for(int j=0; j<_NStreams; j++){
         DatesVector Dates=_Path->GetPath(_Process);
-        PayOffs->AddValue(_Option->GetPayOff(Dates));
-        if(_AntitheticVariable==true){
+        double PayOff=_Option->GetPayOff(Dates);
+        if(_AntitheticVariable==false)
+            PayOffs->AddValue(PayOff);
+        else{
             DatesVector AntitheticDates=_Path->GetAntitheticPath(_Process);
-            PayOffs->AddValue(_Option->GetPayOff(AntitheticDates));
+            double AntitheticPayOff=_Option->GetPayOff(AntitheticDates);
+            PayOffs->AddValue(0.5*(PayOff+AntitheticPayOff));
         }
     }
 };
